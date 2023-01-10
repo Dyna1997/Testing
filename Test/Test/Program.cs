1ad2863dc7b994dc78ac01810285560e7658d163
Max <Zhuqy97@gmail.com>
2023-01-11 01:27:10 +1100

    first commit

---
+++ Test/Test/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+//Open chrome browser
+IWebDriver driver = new ChromeDriver();
+driver.Manage().Window.Maximize();
+
+//Launch Turnup portal
+driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login?ReturnUrl=%2f");
+
+// identify username textbox and enter valid username
+IWebElement usernameTextbox = driver.FindElement(By.Id("UserName"));
+usernameTextbox.SendKeys("hari");
+
+// identify password textbox and enter valid password
+IWebElement passwordTextbox = driver.FindElement(By.Id("Password"));
+passwordTextbox.SendKeys("123123");
+
+//identify login button and click on it
+IWebElement loginButton = driver.FindElement(By.XPath("//*[@id='loginForm']/form/div[3]/input[1]"));
+loginButton.Click();
+
+//identify login button and click on it
+IWebElement helloHari = driver.FindElement(By.XPath("//*[@id='logoutForm']/ul/li/a"));
+
+if (helloHari.Text == "Hello hari!")
+{
+    Console.WriteLine("Logged in successfully, test passed.");
+}
+else
+{
+    Console.WriteLine("Login failed, test failed.");
+}
+
